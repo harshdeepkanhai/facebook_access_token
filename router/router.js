2daf9b28dee206e77aa96479a6857cbeb6499f46
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-03-28 15:06:37 +0530

    add routes

--- router/router.js
+++ router/router.js
@@ -1,7 +1,7 @@
-const firebase = require('../controllers/firebase');
-const sendgrid = require('../controllers/mail');
-const twilio = require('../controllers/message');
-const twitter = require('../controllers/twitter');
+// const firebase = require('../controllers/firebase');
+// const sendgrid = require('../controllers/mail');
+// const twilio = require('../controllers/message');
+// const twitter = require('../controllers/twitter');
 const facebook = require('../controllers/facebook');
 
 module.exports = function(app) {
@@ -10,9 +10,9 @@ module.exports = function(app) {
   });
 
   // Handle API's
-  app.use('/firebase', firebase);
-  app.use('/sendgrid', sendgrid);
-  app.use('/twilio', twilio);
-  app.use('/twitter', twitter);
+  // app.use('/firebase', firebase);
+  // app.use('/sendgrid', sendgrid);
+  // app.use('/twilio', twilio);
+  // app.use('/twitter', twitter);
   app.use('/facebook', facebook);
 };
